9989ca7b3923aef76cd36a27e818a354e9dd77fa
Hugofriedrich369 <hungos0223@gmail.com>
2023-05-04 22:20:40 -0300

    [frontend] Listar-produtos atualizado"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { HomeComponent } from './components/home/home.component';
 import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
 import { CadastrarProdutosComponent } from './components/produtos/cadastrar-produtos/cadastrar-produtos.component';
 
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +29,9 @@ import { CadastrarProdutosComponent } from './components/produtos/cadastrar-prod
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -17,12 +17,11 @@ export class HomeComponent  {
   nomeCompleto: string = `Nome do funcionario: ${this.nomeFunc} ${this.sobrenomeFunc}`;
   nomeProduto: string = "Curso de Angular";
   anuncio: string = `O ${this.nomeProduto} está em promoção`;
-  idProduto: number = 123;
+  idProduto: number = 3;
   precoProduto: number = 2.59;
   promocao: boolean = false;
   foto:string = 'assets/img/crud.png'
-
-
+  dataValidade = '2021-12-31'
 
     constructor() {
 
